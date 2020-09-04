20d11cfe1864c26eaea1d36a581e85e5950ff071
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2020-09-04 19:21:52 +0000

    Added Client Port Class (#1)

---
+++ AUAC_TYPEDEFS/AUAC_TYPES.h
@@ -0,0 +1,82 @@
+/*Copyright (c) 2020 AUAC-Technologies
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+//Author - Nyameaama Gambrah 
+
+#ifndef DEFINITIONS_
+#define DEFINITIONS_
+
+#include<stdint.h>
+#include<stddef.h>
+
+//Void type
+typedef void NO_RETURN;
+
+
+//Unsigned
+
+//Unsigned 8 bit integer
+typedef uint8_t AUAC_UBASE_8;
+//Unsigned 16 bit integer
+typedef uint16_t AUAC_UBASE_16;
+//Unsigned 32 bit integer
+typedef uint32_t AUAC_UBASE_32;
+//Unsigned 64 bit integer
+typedef uint64_t AUAC_UBASE_64;
+
+
+
+//Signed
+
+//8 bit integer
+typedef int8_t AUAC_BASE_8;
+//16 bit integer
+typedef int16_t AUAC_BASE_16;
+//32 bit integer
+typedef int32_t AUAC_BASE_32;
+//64 bit integer
+typedef int64_t AUAC_BASE_64;
+
+
+
+//floating point
+
+//double
+typedef double AUAC_BASE_DOUBLE;
+//float
+typedef float AUAC_BASE_FLOAT;
+
+
+
+//Char
+
+//char*
+typedef char* AUAC_BASE_STRING;
+
+
+
+//Boolean
+typedef bool AUAC_BASE_BOOL;
+
+
+
+
+
+#endif  
